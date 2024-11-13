d4d04445b10a43a798a8a1761e591a79912af8af
ramblaAPPs <gestio@rambla.cat>
2024-11-13 19:24:39 +0100

    Update getLatestPost.js

--- pages/api/getLatestPost.js
+++ pages/api/getLatestPost.js
@@ -1,9 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
 import { getSpotifyToken, fetchLatestPost } from '../../lib/spotify';
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
   const { artistId } = req.query;
@@ -16,44 +11,17 @@ export default async function handler(req, res) {
     // Obtener token de autenticación
     const token = await getSpotifyToken();
 
-    // Obtener la última publicación del artista
+    // Obtener la última publicación del artista en Spotify
     const latestPost = await fetchLatestPost(artistId, token);
 
     if (!latestPost) {
       return res.status(404).json({ message: 'No posts found' });
     }
 
-    // Preparar los datos para la inserción
-    const { name, album_type, release_date, external_urls, images } = latestPost;
-    const dataToInsert = {
-      artist_id: artistId,
-      title: name,
-      type: album_type === 'single' ? 'Canción' : album_type === 'album' ? 'Álbum' : 'EP',
-      release_date: release_date,
-      url: external_urls.spotify,
-      image_url: images[0]?.url || null,
-    };
-
-    try {
-      // Insertar los datos en Supabase
-      const { data, error } = await supabase
-        .from('search_results')
-        .insert([dataToInsert]);
-
-      if (error) {
-        console.error('Error inserting data into Supabase:', error);
-        return res.status(500).json({ error: 'Error inserting data into database', details: error });
-      }
-
-      // Responder con la última publicación
-      res.status(200).json({ latestPost, insertedData: data });
-    } catch (supabaseError) {
-      console.error('Supabase insertion error:', supabaseError);
-      res.status(500).json({ error: 'Error with Supabase insertion', details: supabaseError });
-    }
+    // Responder con la última publicación
+    res.status(200).json(latestPost);
   } catch (error) {
     console.error('Error fetching latest post:', error);
-    res.status(500).json({ error: 'Error fetching the latest post', details: error });
+    res.status(500).json({ error: 'Error fetching the latest post' });
   }
 }
-
