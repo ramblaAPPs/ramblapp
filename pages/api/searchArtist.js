01fc11cd5a8e0fb184dd034ac650d62eccd8a92f
ramblaAPPs <gestio@rambla.cat>
2024-11-13 17:56:29 +0100

    Update searchArtist.js

--- pages/api/searchArtist.js
+++ pages/api/searchArtist.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   if (!query) {
     return res.status(400).json({ error: 'Query is required' });
   }
-
   try {
     // Obtener token de autenticación
     const token = await getSpotifyToken();
@@ -20,7 +19,6 @@ export default async function handler(req, res) {
         },
       }
     );
-
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
